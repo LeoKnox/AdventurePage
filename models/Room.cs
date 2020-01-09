00d16548240cfaead1384c032d946f582a70dfb0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-08 19:14:35 -0700

    Add files via upload

--- models/Room.cs
+++ models/Room.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace AdventurePage.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string RoomName { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
